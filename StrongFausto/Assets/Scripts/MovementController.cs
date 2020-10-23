bc99ccf99f99b5355c641e0d64e4eae6f6d7f95c
Fausto Davila <diukescd@gmail.com>
2020-10-23 17:16:58 +0200

    Basic level test

---
+++ StrongFausto/Assets/Scripts/MovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        SetProperties(1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb.AddForce(Vector3.up * speed);
+    }
+
+    void SetProperties(float speed)
+    {
+        this.speed = speed;
+    }
+}
---
+++ StrongFausto/Assets/Scripts/ShapeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeController : MonoBehaviour
+{
+    public float speed = 0.3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaVertical = 0f;
+        float deltaHorizontal = 0f;
+
+        if (Input.GetKey(KeyCode.W))
+            deltaVertical = 1.0f;
+        else if (Input.GetKey(KeyCode.S))
+            deltaVertical = -1.0f;
+
+        if (Input.GetKey(KeyCode.D))
+            deltaHorizontal = 1.0f;
+        else if (Input.GetKey(KeyCode.A))
+            deltaHorizontal = -1.0f;
+
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x + deltaHorizontal * speed, pos.y, pos.z + deltaVertical * speed);
+    }
+}
